3eac91dcd687ce82f55bc79754e41a37308ab5d9
Sonia Tedesco <sonted@alu.edu.gva.es>
2023-12-03 15:45:04 +0100

    Programa colaborador

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main (String[] args){
-        int C1, C2, C3, C4, S, P;  // Declaro las variables de tipo entero
+        int C1, C2, C3, C4,R;  // Declaro las variables de tipo entero
 
         Scanner leer = new Scanner(System.in); // preparo el programa para leer desde el teclado
 
@@ -19,11 +19,10 @@ public class Ejercicio1 {
         System.out.print("Introduce el cuarto número ");
         C4 = leer.nextInt();
 
-        S = C1 + C2 + C3 + C4;  // Suma de todas las notas
+        R= C1 - C2 - C3 - C4;  // Suma de todas las notas
 
-        P = S / 4;
 
-        System.out.println("El resultado de la media es:" +P);
+        System.out.println("El resultado de la resta es:" +R);
 
 
     }
---
+++ src/colaboradora.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class colaboradora {
+    public static void main (String[] args){
+        System.out.println("Hola");
+        System.out.println("Soy tu colaboradora y he efectuado cambios en tu código");
+        System.out.println("Un saludo");
+    }
+}
